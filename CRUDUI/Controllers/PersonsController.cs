561464999cb76fb83a7bf1835dc8c0482e954dee
AliDabwan <dev.alidabwan@gmail.com>
2023-12-09 20:24:25 +0300

    ui

---
+++ CRUDUI/Controllers/PersonsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CRUDUI.Controllers
+{
+    public class PersonsController : Controller
+    {
+        [Route("/")]
+        [Route("persons")]
+        [Route("persons/index")]
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
